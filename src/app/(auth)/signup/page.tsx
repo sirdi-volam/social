0610a7da641a428895c07c7d75115edd746d5476
sirdi <sirdi-volam@gmail.com>
2024-11-25 19:17:38 +0300

    add signup form, button, loading button

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import signupImage from "@/assets/unnamed.jpg";
 import Image from "next/image";
 import Link from 'next/link';
+import SignUpForm from './SignUpForm';
 
 export const metadata: Metadata = {
   title: "Зарегистрироваться",
@@ -20,7 +21,7 @@ export default function Page() {
             </p>
           </div>
           <div className="space-y-5">
-            Форма регистрации
+            <SignUpForm />
             <Link href="/login" className="block text-center hover:underline">
               Уже есть аккаунт? Войти
             </Link>
